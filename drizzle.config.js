a0ef58df86282dac42260c5921736f8efcbd76f9
Fahri kafalı <f221220035@ktun.edu.tr>
2026-02-27 23:18:53 +0300

    refactor: remove drizzle ORM and related migrations

--- drizzle.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("drizzle-kit").Config} */
-module.exports = {
-  schema: "./src/db/schema.ts",
-  out: "./drizzle",
-  dialect: "postgresql",
-  dbCredentials: {
-    url:"postgresql://dayframe:dayframe@localhost:5432/dayframe",
-  },
-};
